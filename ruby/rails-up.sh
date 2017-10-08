a708741a30db56fdb24de76111cbeb66930604cb
Taufek Johar <taufek@gmail.com>
2017-10-09 06:21:29 +0800

    Add rails-new test
    Also move all scripts to a folder to facilitate COPY command during build.

--- ruby/rails-up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm tmp/pids/server.pid
-bundle check || bundle install
-rails s -b 0.0.0.0
--- travis-build.sh
+++ travis-build.sh
@@ -11,10 +11,16 @@ env | sort
 
 #### Build the Docker Images
 if [ -n "${RUBY_VERSION}" ]; then
-    cp env.example .env
-    sed -i -- "s/RUBY_VERSION=.*/RUBY_VERSION=${RUBY_VERSION}/g" .env
-    sed -i -- 's/=false/=true/g' .env
-    cat .env
-    docker-compose build ${BUILD_SERVICE}
-    docker images
+  cp env.example .env
+  sed -i -- "s/RUBY_VERSION=.*/RUBY_VERSION=${RUBY_VERSION}/g" .env
+  sed -i -- "s/RUBY_RAILS_VERSION=.*/RUBY_RAILS_VERSION=${RUBY_RAILS_VERSION}/g" .env
+  sed -i -- 's/=false/=true/g' .env
+
+  cat .env
+
+  docker-compose build ${BUILD_SERVICE}
+  docker images
+
+  docker-compose run ruby gem install rails -v $RUBY_RAILS_VERSION
+  docker-compose run ruby rails-new
 fi
