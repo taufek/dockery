6e2f07e0bb7cb93ae96e48f0296d98daf290f62a
Taufek Johar <taufek@gmail.com>
2017-10-09 00:15:06 +0800

    Add travis build

---
+++ travis-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#### halt script on error
+set -xe
+
+echo '##### Print docker version'
+docker --version
+
+echo '##### Print environment'
+env | sort
+
+#### Build the Docker Images
+if [ -n "${RUBY_VERSION}" ]; then
+    cp env-example .env
+    sed -i -- "s/RUBY_VERSION=.*/RUBY_VERSION=${RUBY_VERSION}/g" .env
+    sed -i -- 's/=false/=true/g' .env
+    cat .env
+    docker-compose build ${BUILD_SERVICE}
+    docker images
+fi
